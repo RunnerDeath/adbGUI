3fd5b310d9bb94c8d9af8622bc7f4439a27305b9
av.gustov <av.gustov@tensor.ru>
2023-09-27 14:08:51 +0300

    Доработки

--- main.py
+++ main.py
@@ -3,33 +3,41 @@ from adb_functions import *
 
 
 def install_error_popup():
-    sg.popup("Выберите устройства для выполнения дальнейших действий", title='Список устройств пуст .')
+    sg.popup("Выберите устройства для выполнения дальнейших действий", title='Список устройств пуст .', location=(0, 0))
 
 
 device = []
 apk_file = []
+package = []
 layout = [
-    [sg.Text('Выбранное Android-устройства'),
-     sg.InputText(key='_ACTUAL_DEVICE_',readonly=True),
+    [sg.Text(' Android устройство'),
+     sg.InputText(key='_ACTUAL_DEVICE_', readonly=True, expand_x=True),
      sg.Button('Очистить', key='_CLEAR_')],
-    [sg.Button('Демонстрация экрана', key='_SHOW_SCREEN_')],
-    [sg.Listbox(key='_DEV_LST_', values=devices_list(), size=(0, 5), enable_events=True,
+    [sg.Listbox(key='_DEV_LST_', values=devices_list(), size=(0, 3), enable_events=True,
                 select_mode=sg.LISTBOX_SELECT_MODE_SINGLE, auto_size_text=True),
-     sg.Listbox(key='_APP_LST_', values=[], size=(30, 5), enable_events=True,
-                select_mode=sg.LISTBOX_SELECT_MODE_SINGLE),
-     sg.Button('Удалить приложение', key='_DELETE_')],
-
-    [sg.Button('Выбрать APK-файл', key='_SELECT_'),
-     sg.InputText(key='_APK_FILE_'),
-     sg.Button('Установить APK-файл', key='_INSTALL_')]
+     sg.Listbox(key='_PKG_LST_', values=[], enable_events=True,
+                select_mode=sg.LISTBOX_SELECT_MODE_SINGLE, auto_size_text=True, expand_x=True, expand_y=True),
+     sg.Button('Удалить приложение', key='_DELETE_', size=(10, 2))],
+    [sg.Button('Демонстрация экрана', key='_SHOW_SCREEN_', size=(20, 1))],
+    [sg.Button('Выбрать APK-файл', key='_SELECT_', size=(20, 1)),
+     sg.InputText(key='_APK_FILE_', readonly=True,expand_x=True)],
+    [sg.Button('Установить APK-файл', key='_INSTALL_', size=(20, 1))]
 ]
-window = sg.Window('Test').Layout(layout)
+window = sg.Window('adbGUI').Layout(layout)
+
+
+def update_package_list():
+    window.Element('_PKG_LST_').Update(app_list(device))
+
+
 while True:
     event, values = window.read()
     if event == '_DEV_LST_' and len(values['_DEV_LST_']):
         window.Element('_ACTUAL_DEVICE_').Update(values['_DEV_LST_'])
         device = values['_DEV_LST_']
-        window.Element('_APP_LST_').Update(app_list(device))
+        update_package_list()
+    if event == '_PKG_LST_' and len(values['_PKG_LST_']):
+        package = values['_PKG_LST_']
     if event == '_SHOW_SCREEN_':
         if len(device) == 0:
             sg.popup("Список устройств пуст. Выберите устройства для выполнения дальнейших действий")
@@ -49,13 +57,16 @@ while True:
     if event == '_INSTALL_':
         if len(device) > 0 and len(apk_file) > 0:
             r = install_apk(device, apk_file)
-            for i in range(0, len(r)):
-                sg.popup(f'{r[i]}', title='Информационное окно')
+            sg.popup(f'{r}', title='Информационное окно')
         else:
             install_error_popup()
     if event == '_DELETE_':
-        if len(device) > 0 and len(apk_file) > 0:
-            sg.popup('Surprice')
+        if len(device) > 0 and len(package) > 0:
+            r = uninstall_apk(device, package)
+            sg.popup(f'{r}', title='Информационное окно')
+            update_package_list()
+        else:
+            install_error_popup()
     if event == sg.WIN_CLOSED:
         break
 
